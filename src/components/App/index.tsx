8e67028038b89e341ad7afa99d980ab1cb2ccdc3
gabrielcaamargo <gcamargo.dev@gmail.com>
2023-01-06 13:04:09 -0300

    🔥 feat: set react router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,27 +1,30 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ThemeProvider } from 'styled-components';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import { useState } from 'react';
 
-import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/GlobalStyles';
-import Header from '../Header';
-
-import { ChakraProvider } from '@chakra-ui/react';
-
 import darkTheme from '../../assets/styles/themes/darkTheme';
 import lightTheme from '../../assets/styles/themes/lightTheme';
 
-import { ThemeContext } from '../../contexts/ThemeContext';
+import Header from '../Header';
+import Router from '../../Router';
 
 export default function App() {
   const [theme, setTheme] = useState('dark');
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-        <ChakraProvider>
-          <GlobalStyles />
-          <Header />
-        </ChakraProvider>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <BrowserRouter>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+          <ChakraProvider>
+            <GlobalStyles />
+            <Header />
+            <Router />
+          </ChakraProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </BrowserRouter>
   );
 }
